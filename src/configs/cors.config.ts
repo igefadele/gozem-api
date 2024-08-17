d76fa35cf32a06eae513297ccaa6dd49256e096c
Amos <igefad12@gmail.com>
2024-08-17 21:12:11 +0100

    UPDATE: Cors origin updated

--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -7,7 +7,17 @@ import cors from 'cors';
 import { GET, POST, PUT, DELETE } from '../core/constants';
 import { BASE_URL } from './env.config';
 
-export const corsConfig = cors({
-  origin: BASE_URL,
+export const allowedOrigins = [BASE_URL, "http://localhost:4200"]
+
+export const corsOption = {
+  origin: (origin: string | undefined, callback: (error: Error | null, allowed?: boolean) => void) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'), false);
+    }
+  },
   methods: [GET, POST, PUT, DELETE],
-})
+}
+
+export const corsConfig = cors(corsOption);
--- src/configs/socketio.config.ts
+++ src/configs/socketio.config.ts
@@ -8,16 +8,12 @@ import { Server } from 'socket.io';
 import { CONNECTION, DELETE, DISCONNECT, GET, POST, PUT, SOCKETIO_CONNECTED, SOCKETIO_DISCONNECTED } from '../core/constants';
 import { BASE_URL } from './env.config';
 import { handleIncomingEvent } from '../core/websocket/websocket';
+import { corsOption } from './cors.config';
 
 let io: Server;
 
 export const initializeSocketIO = (server: HttpServer) => {
-  io = new Server(server, {
-    cors: {
-      origin: BASE_URL,
-      methods: [GET, POST, PUT, DELETE],
-    }
-  });
+  io = new Server(server, {cors: corsOption});
 
   io.on(CONNECTION, (socket) => {
     console.log(SOCKETIO_CONNECTED);
